04a92f81bc5cc12c0d72dcf7b10e2c3fa12b698e
Syrus Akbary <me@syrusakbary.com>
2015-10-07 20:36:45 -0700

    Updated setup for include static and template files

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 from setuptools import setup, find_packages
 
-setup(name='django-graphiql',
-      version='0.3.1',
-      download_url='git@github.com:graphql-python/django-graphiql.git',
-      packages=find_packages(),
-      author='Syrus Akbary',
-      author_email='me@syrusakbary.com',
-      description='Integrate GraphiQL easily into your Django project',
-      long_description=open('README.rst').read(),
-      keywords='django graphiql graphql graphene graphql-core',
-      url='http://github.com/graphql-python/django-graphiql',
-      license='MIT',
-      entry_points={
-      },
-      install_requires=['django>=1.7'],
-      tests_require=[])
+setup(
+    name='django-graphiql',
+    version='0.3.1.5',
+    download_url='git@github.com:graphql-python/django-graphiql.git',
+    packages=find_packages(),
+    author='Syrus Akbary',
+    author_email='me@syrusakbary.com',
+    description='Integrate GraphiQL easily into your Django project',
+    long_description=open('README.rst').read(),
+    keywords='django graphiql graphql graphene graphql-core',
+    url='http://github.com/graphql-python/django-graphiql',
+    license='MIT',
+    entry_points={
+    },
+    install_requires=['django>=1.7'],
+    tests_require=[],
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+)
