45447716c54f3d3e45d675b6220ee5c338c4ca50
Syrus Akbary <me@syrusakbary.com>
2015-10-14 16:07:30 -0700

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-graphiql',
-    version='0.3.1.7',
+    version='0.3.1.8',
     download_url='git@github.com:graphql-python/django-graphiql.git',
     packages=find_packages(),
     author='Syrus Akbary',
